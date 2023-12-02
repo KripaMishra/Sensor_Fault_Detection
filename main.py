7f680dbf634b21385c3482a036272961e03f843d
KripaMishra <kripa5661@gmail.com>
2023-12-03 00:07:29 +0530

    error

---
+++ main.py
@@ -0,0 +1,11 @@
+from sensor.configuration.mongo_db_connection import MongoDBClient
+from sensor.exception import SensorException
+import os,sys
+from sensor.logger import logging 
+from sensor.entity.config_entity import TrainingPipelineConfig
+
+
+if __name__=='__main__':
+    training_pipeline_config= TrainingPipelineConfig()
+    data_ingestion_config= DataIngestionConfig(training_pipeline_config=training_pipeline_config)
+    print(data_ingestion_config.__dict)
